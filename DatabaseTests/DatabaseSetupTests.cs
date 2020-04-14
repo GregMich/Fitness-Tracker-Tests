1ecf85361c975857776276bfc7647bfe5bddfcc6
greg-mich <gregmich95@gmail.com>
2020-04-14 02:17:24 -0500

    added test database startup helper to seed in memory database with test data, added a unit test to test UserProfileController with mocked Interface for ClaimsManager

---
+++ DatabaseTests/DatabaseSetupTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using TestSupport.EfHelpers;
+using System.Linq;
+using Fitness_Tracker.Data.Contexts;
+using Fitness_Tracker_Tests.TestSetup;
+
+namespace Fitness_Tracker_Tests.DatabaseTests
+{
+    public class DatabaseSetupTests
+    {
+
+        [Fact]
+        public void TestSqliteInMemoryOk()
+        {
+            var options = SqliteInMemory
+                .CreateOptions<ApplicationDbContext>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.SeedTestDatabaseUsers();
+
+                var users = context
+                    .Users
+                    .ToList();
+
+                Assert.Equal(3, users.Count());
+            }
+        }
+    }
+}
